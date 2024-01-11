320820fc8e62dbf9eddab59b007c2634728b44c3
Mustafa J <mustafa.0x@gmail.com>
2024-01-10 21:35:28 -0800

    remote_added: use `:` as host:path sep

--- remote_added.py
+++ remote_added.py
@@ -4,15 +4,14 @@
 Determines which files on remote aren't tracked by local nor ignored.
 Useful when deploying with tar without removing old files.
 
-Usage: python remote_added.py <host> <remote_dir>
+Usage: python remote_added.py <host>:<remote_dir>
 """
 from subprocess import run as run_
 import sys
 
 run = lambda cmd, **kwargs: run_(cmd, capture_output=True, encoding='utf-8', **kwargs)
 
-host = sys.argv[1]
-remote_dir = sys.argv[2]
+host, remote_dir = sys.argv[1].split(':')
 git_files = run(['git', 'ls-files']).stdout[:-1].split('\n')
 remote_files = run(f'ssh {host} "find {remote_dir} -type f"', shell=True)
 remote_files = remote_files.stdout[:-1].replace(f'{remote_dir.rstrip("/")}/', '').split('\n')
