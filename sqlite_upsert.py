72e78fbc082995d55410e8d99b8a7a2524204de7
Mustafa J <mustafa.0x@gmail.com>
2024-02-14 12:04:45 -0800

    sqlite_upsert: fixes and updates

--- sqlite_upsert.py
+++ sqlite_upsert.py
@@ -18,21 +18,28 @@ args = parser.parse_args()
 #####################################################################
 # Main
 #####################################################################
-def insert(db, d):
-    stmt = f'INSERT OR REPLACE INTO {d["table"]} ({", ".join(d["data"].keys())}) VALUES ({", ".join(["?"] * len(d["data"]))})'
-    db.execute(stmt, tuple(d["data"].values()))
+make_stmt = lambda d: (  # noqa: E731
+    f'INSERT OR REPLACE INTO {d["table"]} ({", ".join(d["data"].keys())}) VALUES ({", ".join(["?"] * len(d["data"]))})',
+    tuple(d["data"].values()),
+)
 
 db = sqlite3.connect(args.db)
 data_source = args.file.open() if args.file else sys.stdin
+data = None
 if args.lines:
-    for line in data_source:
-        insert(db, json.loads(line))
+    data = (json.loads(line) for line in data_source)
 else:
     data = json.load(data_source)
     if isinstance(data, dict):
+        # The json file is an object, but data needs to be a list
         data = [data]
 
-    for d in data:
-        insert(db, d)
+for d in data:
+    # Always assume that we we need to update many rows for a single table
+    if isinstance(d['data'], dict):
+        d['data'] = [d['data']]
+
+    for p in d['data']:
+        db.execute(*make_stmt({'table': d['table'], 'data': p}))
 
 db.commit()
